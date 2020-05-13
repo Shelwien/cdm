9a0b1a5233eeec60826ce388826ae0cbd578eb3f
Eugene D. Shelwien <shelwien@gmail.com>
2020-05-13 08:14:02 +0300

    commit

--- test.bat
+++ test.bat
@@ -1,12 +1,12 @@
 @echo off
 
-bwth c A10.jpg A10.bwth
+bwth c5000 A10.jpg A10.bwth
 
 cdm c A10.bwth A10.bwth.cdm
 
 cdm d A10.bwth.cdm A10.bwth.unp
 
-bwth d A10.bwth.unp A10.unp
+bwth d5000 A10.bwth.unp A10.unp
 
 fc /b A10.jpg A10.unp
 
